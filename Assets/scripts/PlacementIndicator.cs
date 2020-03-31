6de385c30f7fa292e9ff790dd62b869ad94897a8
MadHatter01 <r.vaishnavi15@gmail.com>
2020-03-30 20:05:20 -0700

    Added Chair placement

---
+++ Assets/scripts/PlacementIndicator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlacementIndicator : MonoBehaviour
+{
+
+    private ARRaycastManager arRayCastManager;
+    private GameObject indicator;
+
+    void Start()
+        {
+            arRayCastManager = FindObjectOfType<ARRaycastManager>();
+            indicator = GameObject.FindGameObjectWithTag("indicator");
+            indicator.SetActive(false);
+        }
+
+    void Update()
+        {
+            List<ARRaycastHit> hits = new List<ARRaycastHit>();
+            arRayCastManager.Raycast(new Vector2(Screen.width/2, Screen.height/2), hits, TrackableType.Planes);
+
+            if (hits.Count > 0)
+            {
+                transform.position = hits[0].pose.position;
+                transform.rotation = hits[0].pose.rotation;
+             
+                if(!indicator.activeInHierarchy)
+                {
+                    indicator.SetActive(true);
+                }
+            }
+        }
+}
---
+++ Assets/scripts/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private PlacementIndicator indicator;
+    public GameObject ObjectToPlace;
+
+    void Start()
+        {
+            indicator = FindObjectOfType<PlacementIndicator>();
+        }
+
+    void Update()
+        {
+            if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+            {
+            Debug.Log("Instantiated)");
+                GameObject obj = Instantiate(ObjectToPlace, indicator.transform.position, indicator.transform.rotation);
+            }
+        }
+}
